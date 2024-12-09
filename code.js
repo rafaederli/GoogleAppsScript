d30696b086224d90b4792f27f1aa756e4ee4ed62
Rafael Ederli <rafaederli@gmail.com>
2024-12-09 07:54:13 -0300

    pass objects and template

--- code.js
+++ code.js
@@ -1,10 +1,18 @@
-function doGet(e) {
-  
-    Logger.log(e)
-    return HtmlService.createHtmlOutputFromFile("page");
-  }
-  
-  
-  function userClicked(name) {
-    Logger.log(name + " clicked the Button");
-  }
+function doGet() {
+  return HtmlService.createTemplateFromFile("page").evaluate();
+  // return HtmlService.createHtmlOutputFromFile("page");
+}
+
+function userClicked(userInfo) {
+  var url = "https://docs.google.com/spreadsheets/d/1cpt1IWMsd06lVaGumjMo7phc_B17wxsVAHf7ujOGTfo/edit?gid=0#gid=0";
+  var ss = SpreadsheetApp.openByUrl(url);
+  var ws = ss.getSheetByName("Data");
+
+  ws.appendRow([userInfo.firstName, userInfo.lastName, userInfo.app, new Date()]);
+
+  // Logger.log(name + " clicked the Button");
+}
+
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
