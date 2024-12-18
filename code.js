ef4313ddc28666005ff58ad4b09e531203598195
Rafael Ederli <rafaederli@gmail.com>
2024-12-17 23:14:56 -0300

    load data

--- code.js
+++ code.js
@@ -1,10 +1,18 @@
+var url = "https://docs.google.com/spreadsheets/d/1cpt1IWMsd06lVaGumjMo7phc_B17wxsVAHf7ujOGTfo/edit?gid=0#gid=0";
+
 function doGet() {
-  return HtmlService.createTemplateFromFile("page").evaluate();
+  
+  var ss = SpreadsheetApp.openByUrl(url);
+  var ws = ss.getSheetByName("Options");
+  var list = ws.getRange(1,1,ws.getRange("A1").getDataRegion().getLastRow(),1).getValues();
+  
+  var tmp = HtmlService.createTemplateFromFile("page");
+  tmp.list = list.map(function(r){return r[0]});
+  return tmp.evaluate();
   // return HtmlService.createHtmlOutputFromFile("page");
 }
 
 function userClicked(userInfo) {
-  var url = "https://docs.google.com/spreadsheets/d/1cpt1IWMsd06lVaGumjMo7phc_B17wxsVAHf7ujOGTfo/edit?gid=0#gid=0";
   var ss = SpreadsheetApp.openByUrl(url);
   var ws = ss.getSheetByName("Data");
 
