812aecada37b3cd973d931e8a62ba31bd5a988c6
Rafael Ederli <rafaederli@gmail.com>
2024-12-18 21:32:35 -0300

    search match in spreadsheet

--- code.js
+++ code.js
@@ -16,9 +16,23 @@ function userClicked(userInfo) {
   var ss = SpreadsheetApp.openByUrl(url);
   var ws = ss.getSheetByName("Data");
 
-  ws.appendRow([userInfo.firstName, userInfo.lastName, userInfo.app, new Date()]);
+  ws.appendRow([userInfo.firstName, userInfo.lastName, userInfo.app, userInfo.zip, userInfo.est, new Date()]);
 
   // Logger.log(name + " clicked the Button");
+}
+
+function getCost(zipCode) {
+  var ss = SpreadsheetApp.openByUrl(url);
+  var ws = ss.getSheetByName("Estimate");
+  var data = ws.getRange(1,1,ws.getLastRow(),2).getValues();
+  var zipCodeList = data.map(function(r){return r[0];});
+  var costList = data.map(function(r){return r[1];});
+  var position = zipCodeList.indexOf(zipCode)
+  if(position > -1) {
+    return "$" + costList[position].toFixed(2);
+  } else {
+    return "Unavailble"
+  }
 }
 
 function include(filename) {
